d0705bdb8cda4cccef4e3f74b29b7e16d66178e4
ievaSW <149368184+ievaSW@users.noreply.github.com>
2024-04-22 17:00:19 +0300

    test correction

--- tests/PipeCombinations.spec.ts
+++ tests/PipeCombinations.spec.ts
@@ -1,48 +1,56 @@
 import {expect, test} from '@playwright/test';
 
-import { RandomString } from '../testData';
-
-
-
-
 test.describe(`Pipes`,  () => {
     const combinations =[
         [1,2,3,4,5],
-        [2,3,4,5],
+        [1,3,4],
+        [1,3,5],
+        [1,2,3],
+        [1,2,4,5],
+        [1,2,4],
+        [1,2,5],
+        [1,2],
         [1,3,4,5],
-        ]
-       
+        [1],
+        [2,3,4,5],
+        [2,3,4],
+        [2,4,5],
+        [2,3,5],
+        [2,3],
+        [2],
+        [2,5],
+        [2,4],
+        [3,5],
+        [3],
+        [3,4,5],
+        [3,4],
+        [4],
+        [5]
+        ];
 
-        for (const combination of combinations) {
-            test(`Should be able to turn on ${combination.toString}`, async ({page})=>{
-                await page.goto('/testing/WaterFlowSimulation/');
-        
-                await page.locator('#valve1').click()
-                await expect(page.locator('#pipe1')).toHaveClass('pipe active')
-        
-                await page.locator('#valve2').click()
-                await expect(page.locator('#pipe2')).toHaveClass('pipe active')
-        
-                await page.locator('#valve3').click()
-                await expect(page.locator('#pipe3')).toHaveClass('pipe active')
-        
-                await page.locator('#valve4').click()
-                await expect(page.locator('#pipe4')).toHaveClass('pipe active')
-        
-                await page.locator('#valve5').click()
-                await expect(page.locator('#pipe5')).toHaveClass('pipe active')
-                
+for (const combination of combinations) {
+    console.log(combination)
+    test(`Should be able to turn on ${combination.toString()} pipes`, async ({page})=>{
+        await page.goto('/testing/WaterFlowSimulation');
+
+        for (const pipes of combination){
+            await page.click(`#valve${pipes}`)
+                }
+        for (const pipes of combination){
+            await expect(page.locator(`#pipe${pipes}`)).toHaveClass('pipe active')
+                }
             })
         }
-    
 })
 
 test.describe(`Timer`,  () => {
     test('Detecting time', async ({page})=>{
         await page.goto('https://testingmarathon.com/testing/Timer/');
-        await page.locator('#start').click()
-        await page.locator('#stop').click()
-        await expect(page.locator('#display')).not.toContainText('00:00:00.04')
+        for(let i=0; i<=5; i++){
+            await page.locator('#start').click()
+            await page.locator('#stop').click()
+        }
+        await expect(page.locator('#display')).not.toContainText('00:00:00.00')
     })
     
-  })
+})
